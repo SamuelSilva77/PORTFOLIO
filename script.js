563e667429aa8a46db6b1fd0218a2cb166d95443
Samuel Silva <samuelpds77@gmail.com>
2026-06-15 19:40:22 -0300

    ADICIONADO PROJETO DE MARGEM DE AÇÕES

--- script.js
+++ script.js
@@ -21,21 +21,24 @@ const dados = [
         nome: "Painel de Produtividade",
         sobre: "Um painel visual de acompanhamento de hábitos que ajuda a monitorar sua consistência diária e progresso ao longo do tempo.",
         tecnologias: ["HTML", "CSS", "JAVASCRIPT", "API REST"],
-        linkCodigo: "https://github.com/SamuelSilva77/PAINEL-DE-PRODUTIVIDADE"
+        linkCodigo: "https://github.com/SamuelSilva77/PAINEL-DE-PRODUTIVIDADE",
+        img: "https://mgx-backend-cdn.metadl.com/generate/images/1178818/2026-05-10/oiweuoaaagoq/project-placeholder-1.png"
     },
     {
-        linkSite: " https://samuelsilva77.github.io/SERVICO-DE-MUDANCAS/",
-        nome: "Landing Page - Serviço de Mudanças",
-        sobre: "Site profissional para empresa de mudanças, apresentando serviços, valores e formas de contato de maneira clara e organizada.",
-        tecnologias: ["HTML", "TAILWIND", "JS"],
-        linkCodigo: "https://github.com/SamuelSilva77/SERVICO-DE-MUDANCAS"
+        linkSite: "https://samuelsilva77.github.io/CALCULADORA-DE-MARGEM-ACOES/",
+        nome: "Calculo de Margem - Ações",
+        sobre: "Aplicação web para análise de ações que calcula margem de compra com base no preço teto definido pelo usuário e na cotação atual. Usa consumo de API e Localstorage.",
+        tecnologias: ["HTML", "CSS", "JS", "API REST"],
+        linkCodigo: "https://github.com/SamuelSilva77/CALCULADORA-DE-MARGEM-ACOES",
+        img: "img/margem.png"
     },
     {
         linkSite: " https://samuelsilva77.github.io/SERVICO-DE-LIMPEZA/",
         nome: "Landing Page - Serviço de Limpeza",
         sobre: "Site institucional de limpeza geral, desenvolvido para apresentar serviços de forma clara e objetiva, facilitar o contato com clientes e transmitir profissionalismo.",
         tecnologias: ["HTML", "TAILWID CSS", "JS"],
-        linkCodigo: "https://github.com/SamuelSilva77/SERVICO-DE-LIMPEZA/"
+        linkCodigo: "https://github.com/SamuelSilva77/SERVICO-DE-LIMPEZA/",
+        img: "https://mgx-backend-cdn.metadl.com/generate/images/1178818/2026-05-10/oiweuoaaagoq/project-placeholder-1.png"
     },
 ]
 
@@ -49,8 +52,10 @@ function criacao(){
 
     containerProjetos.innerHTML += `
         <div class="card">
-            <a href="${this.linkSite}"  style="display: inline-block; height: 228px; overflow: hidden;">
-                <img src="https://mgx-backend-cdn.metadl.com/generate/images/1178818/2026-05-10/oiweuoaaagoq/project-placeholder-1.png" class="imagemProjeto">
+            <a href="${this.linkSite}" target="_blank" style="width: 100%; display: inline-block; height: 228px; overflow: hidden;">
+                <div class="imagemProjeto">
+                    <img src="${this.img}">
+                </div>
             </a>
             <div class="conteudoTextoCard">
                 <p class="nomeProjeto"> ${this.nome} </p>
@@ -58,7 +63,7 @@ function criacao(){
                  <div>
                     ${botoes}
                 </div>
-                <a href="${this.linkCodigo}"class="codigo">
+                <a href="${this.linkCodigo}" target="_blank" class="codigo">
                     <img src="img/github.png" alt="">
                     <p>Código</p>
                 </a>
